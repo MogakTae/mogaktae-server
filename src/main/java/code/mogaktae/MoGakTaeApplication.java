23e20e8ce26679a0b3dc854efe01883df1973df5
xunxxoie <ahh0520@naver.com>
2025-02-11 20:27:36 +0900

    [Feat] Initial Commit

---
+++ src/main/java/code/mogaktae/MoGakTaeApplication.java
@@ -0,0 +1,13 @@
+package code.mogaktae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoGakTaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoGakTaeApplication.class, args);
+	}
+
+}
