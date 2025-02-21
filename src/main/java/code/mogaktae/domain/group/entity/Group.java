e4b6712a8207965a74479b0fe5c37a8d0b960402
xunxxoie <ahh0520@naver.com>
2025-02-21 14:11:45 +0900

    [Feat] 앤티티 관련 코드 작성 - #1

---
+++ src/main/java/code/mogaktae/domain/group/entity/Group.java
@@ -0,0 +1,39 @@
+package code.mogaktae.domain.group.entity;
+
+import code.mogaktae.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Group {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, name = "dead_line")
+    private LocalDate deadLine;
+
+    @Column(nullable = false, name = "daily_problem_count")
+    private Integer dailyProblemCount;
+
+    @Column(nullable = false, name = "penalty_amount")
+    private Long penaltyAmount;
+
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    private List<User> users = new ArrayList<>();
+
+}
---
+++ src/main/java/code/mogaktae/domain/user/entity/User.java
@@ -0,0 +1,44 @@
+package code.mogaktae.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false, name = "repository_url")
+    private String repositoryUrl;
+
+    @Column(nullable = false, name = "profile_img_url")
+    private String profileImgUrl;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private String role;
+}
