98f05b82d786ace6d607145e766da60300d6d3f5
xunxxoie <ahh0520@naver.com>
2025-02-26 23:58:35 +0900

    [Feat] Git Webhook Test commit

--- src/main/java/code/mogaktae/global/security/config/SecurityConfig.java
+++ src/main/java/code/mogaktae/global/security/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/api/v1/auth/**",
-                                "/login"
+                                "/login",
+                                "/github-webhook"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
