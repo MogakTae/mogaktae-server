2af761736d593cc142fe3c01c78a3179934b1d42
xunxxoie <ahh0520@naver.com>
2025-07-04 18:57:22 +0900

    [Fix] 앨라스틱 서치 설정 파일 수정

--- src/main/java/code/mogaktae/global/config/ElasticSearchConfig.java
+++ src/main/java/code/mogaktae/global/config/ElasticSearchConfig.java
@@ -14,7 +14,7 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
     @Override
     public ClientConfiguration clientConfiguration(){
         return ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo("mogaktae-elasticsearch:9200")
                 .build();
     }
 }
