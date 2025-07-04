b4ccc184914fd2229bb4c683d702da01afc3296c
xunxxoie <ahh0520@naver.com>
2025-07-04 19:04:13 +0900

    [Fix] 앨라스틱 서치 설정 파일 수정

--- src/main/java/code/mogaktae/global/config/ElasticSearchConfig.java
+++ src/main/java/code/mogaktae/global/config/ElasticSearchConfig.java
@@ -4,13 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.lang.NonNull;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "code.mogaktae.domain.user.repository")
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
-    @NonNull
     @Override
     public ClientConfiguration clientConfiguration(){
         return ClientConfiguration.builder()
