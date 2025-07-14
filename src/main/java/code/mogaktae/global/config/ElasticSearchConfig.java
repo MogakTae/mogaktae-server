fce45caf4d9977da7dd1bd76df569c9f4d559841
xunxxoie <ahh0520@naver.com>
2025-07-14 18:44:40 +0900

    [Fix] Elastic search Configuration 수정

--- src/main/java/code/mogaktae/global/config/ElasticSearchConfig.java
+++ src/main/java/code/mogaktae/global/config/ElasticSearchConfig.java
@@ -12,7 +12,7 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
     @Override
     public ClientConfiguration clientConfiguration(){
         return ClientConfiguration.builder()
-                .connectedTo("joonseo-elastic-search:9200")
+                .connectedTo("js-elastic-search:9200")
                 .build();
     }
 }
