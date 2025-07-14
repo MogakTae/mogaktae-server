0ac2bdef51e35221839ab4831c6326f1c2da70ad
xunxxoie <ahh0520@naver.com>
2025-07-14 18:43:53 +0900

    [Fix] 컨테이너 명을 확장성 고려하여 수정

--- src/main/java/code/mogaktae/global/config/ElasticSearchConfig.java
+++ src/main/java/code/mogaktae/global/config/ElasticSearchConfig.java
@@ -12,7 +12,7 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
     @Override
     public ClientConfiguration clientConfiguration(){
         return ClientConfiguration.builder()
-                .connectedTo("es:9200")
+                .connectedTo("joonseo-elastic-search:9200")
                 .build();
     }
 }
